27ba54a950e1e4eb67ed6caabcd35a2a701dd81a
Stefano Perenzoni <stefano.perenzoni@unitn.it>
2019-11-21 09:22:14 +0100

    Heroku commit

--- client.py
+++ client.py
@@ -8,6 +8,8 @@ targetJson = json.loads(jsonStr)
 
 #print(targetJson)
 
+# https://tapoimaps.herokuapp.com:5000
+# http://127.0.0.1:5000/similarity?alg=jaccard
 r = requests.get('http://127.0.0.1:5000/similarity?alg=jaccard', json=targetJson)
 
 resp = r.json()
